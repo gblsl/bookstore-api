b6578eb16e176d5e8aabefb9f321f64b7d6b455b
gblsl <gblsl@protonmail.ch>
2021-09-13 14:09:29 -0300

    Finalizando o backend

--- src/main/java/com/gabriel/bookstore/domain/Categoria.java
+++ src/main/java/com/gabriel/bookstore/domain/Categoria.java
@@ -10,16 +10,25 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
 
 @Entity
 public class Categoria implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+
+	@NotEmpty(message = "Campo NOME é requerido")
+	@Length(min = 3, max = 100, message = "O campo NOME deve ter entre três e cem caracteres")
 	private String nome;
+	
+	@NotEmpty(message = "Campo DESCRIÇÃO é requerido")
+	@Length(min = 3, max = 200, message = "O campo DESCRIÇÃO deve ter entre três e duzentos caracteres")
 	private String descricao;
 
 	@OneToMany(mappedBy = "categoria")
