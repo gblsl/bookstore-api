a2187de98800f8672df7eb9c63334893298660c0
gblsl <gblsl@protonmail.ch>
2021-09-13 11:48:34 -0300

    Put e Patch para livro

--- src/main/java/com/gabriel/bookstore/services/LivroService.java
+++ src/main/java/com/gabriel/bookstore/services/LivroService.java
@@ -16,7 +16,7 @@ public class LivroService {
 
 	@Autowired
 	private LivroRepository repository;
-	
+
 	@Autowired
 	private CategoriaService categoriaService;
 
@@ -29,5 +29,17 @@ public class LivroService {
 	public List<Livro> findAll(Integer id_cat) {
 		categoriaService.findById(id_cat);
 		return repository.findAllByCategoria(id_cat);
+	}
+
+	public Livro update(Integer id, Livro obj) {
+		Livro newObj = findById(id);
+		updateData(newObj, obj);
+		return repository.save(newObj);
+	}
+
+	private void updateData(Livro newObj, Livro obj) {
+		newObj.setTitulo(obj.getTitulo());
+		newObj.setNomeAutor(obj.getNomeAutor());
+		newObj.setTexto(obj.getTexto());
 	}
 }
