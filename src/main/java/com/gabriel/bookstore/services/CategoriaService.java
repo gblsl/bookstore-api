0f35f1d7cef1681e960d5241ce54aa8d95deaf59
gblsl <gblsl@protonmail.ch>
2021-09-13 10:34:43 -0300

    Update para categoria

--- src/main/java/com/gabriel/bookstore/services/CategoriaService.java
+++ src/main/java/com/gabriel/bookstore/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gabriel.bookstore.domain.Categoria;
+import com.gabriel.bookstore.dtos.CategoriaDto;
 import com.gabriel.bookstore.repositories.CategoriaRepository;
 import com.gabriel.bookstore.services.exceptions.ObjectNotFoundException;
 
@@ -28,6 +29,13 @@ public class CategoriaService {
 
 	public Categoria create(Categoria obj) {
 		obj.setId(null);
+		return repository.save(obj);
+	}
+
+	public Categoria update(Integer id, CategoriaDto objDto) {
+		Categoria obj = findById(id);
+		obj.setNome(objDto.getNome());
+		obj.setDescricao(objDto.getDescricao());
 		return repository.save(obj);
 	}
 }
